97b06da6cc8a8dc7cf0fc8afe1dca31958210f1f
liuwenwen163 <liuwenwen163@gmail.com>
2019-12-18 15:57:25 +0800

    v0.1 完成了django必要环境的配置

---
+++ app/client/urls.py
@@ -0,0 +1,10 @@
+# encoding: utf-8
+from django.urls import path
+
+__author__ = 'bbw'
+
+
+urlpatterns = [
+
+]
+
---
+++ app/dashboard/urls.py
@@ -0,0 +1,9 @@
+# encoding: utf-8
+
+__author__ = 'bbw'
+
+from django.urls import path
+
+urlpatterns = [
+
+]
